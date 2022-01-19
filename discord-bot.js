98391a6122799a654691b87304ff2b4f19bcea7b
RisingThumb <aaronleonard@risingthumb.xyz>
2022-01-19 17:46:53 +0000

    Added and fixed dice rolls

--- discord-bot.js
+++ discord-bot.js
@@ -1,44 +1,46 @@
-var kill = require('tree-kill');
-var child_process = require('child_process');
+var kill = require("tree-kill");
+var child_process = require("child_process");
 var discord = require("discord.js");
 
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require("./config.json");
 
+function requireUncached(module) {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+}
+
+var {games} = requireUncached("./config.json")
 var bot = new discord.Client();
 require('discord-buttons')(bot);
+var optionsCount = 0;
 
 const disbut = require("discord-buttons");
-var games = [
-    {
-        "label": "SRB2Kart Server",
-        "value": "srb2kart", // Name should be the name of the service excluding the .service part.
-        "emoji": "🏎",
-        "description" : "SRB2Kart"
-    },
-    {
-        "label": "TTT Server",
-        "value": "ttt", // Name should be the name of the service excluding the .service part.
-        "emoji": "💀",
-        "description" : "Double barreled Shotgun Acquired"
-    }
-]
 
-let select = new disbut.MessageMenu()
-    .setID("menuID")
-    .setPlaceholder("Pick a server to run.")
-    .setMaxValues(1)
-    .setMinValues(1);
+let select = null
+reloadGames();
 
-games.forEach((game) => {
-    let option = new disbut.MessageMenuOption()
-        .setLabel(game["label"])
-        .setEmoji(game["emoji"])
-        .setValue(game["value"])
-        .setDescription(game["description"]);
-    select.addOption(option);
-})
+
 
 var latestMessageId = null;
+
+function reloadGames() {
+    select = new disbut.MessageMenu()
+        .setID("menuID")
+        .setPlaceholder("Pick a server to run.")
+        .setMaxValues(1)
+        .setMinValues(1);
+
+    var {games} = requireUncached("./config.json");
+    this.games = games;
+    games.forEach((game) => {
+        let option = new disbut.MessageMenuOption()
+            .setLabel(game["label"])
+            .setEmoji(game["emoji"])
+            .setValue(game["value"])
+            .setDescription(game["description"]);
+        select.addOption(option);
+})
+}
 
 var helpEmbed = new discord.MessageEmbed()
     .setColor("#0099ff")
@@ -48,10 +50,18 @@ var helpEmbed = new discord.MessageEmbed()
     .setDescription("A list of Commands available...")
     .addFields(
         {name: ".help", value: "This helpful page."},
-        {name: ".gaming", value: "A drop down menu for servers that can be run."}
+        {name: ".gaming", value: "A drop down menu for servers that can be run."},
+        {name: ".reload", value: "Reload config. Probably not needed by you."},
+        {name: ".kill", value: "Kills any currently hosted servers. Kindly kill at the end of a session to avoid consuming resources :^)"}
     )
     .setTimestamp()
 	.setFooter(":^)", "https://git.risingthumb.xyz/avatars/70607f1b387ddf9f7822713f8efe97be?size=580");
+
+function killAllServices() {
+    for (var i = 0; i < games.length; i++) {
+        child_process.exec("sudo systemctl stop "+games[i]["value"]+".service");
+    }
+}
 
 bot.on("message", msg => {
     if (msg.content == `${prefix}help`) {
@@ -61,13 +71,52 @@ bot.on("message", msg => {
         latestMessageId = msg.author.id;
         msg.channel.send("Options", select);
     }
+    if (msg.content == `${prefix}kill`) {
+        killAllServices();
+        msg.channel.send("All servers have been closed! Thanks for playing!");
+    }
+    if (msg.content == `${prefix}reload`) {
+        reloadGames();
+        msg.channel.send("Reloaded config!");
+    }
+    if (msg.content.includes(`${prefix}roll`)) {
+        let rolling = msg.content.split(" ");
+        let die = rolling[1];
+        let diceToPrint = []
+        if (die) {
+            let parts = die.split("d");
+            let numberOfDice = parseInt(parts[0]);
+            if (parts.length !== 2) return;
+            let facesOfDie = parseInt(parts[1]);
+            if (numberOfDice > 33) {
+                msg.channel.send("No die rolled. I will not roll more than 33 Dice...")
+                return;
+            }
+            if (numberOfDice && facesOfDie) {
+                for (let i = 0; i < numberOfDice; i++) {
+                    var roll = Math.floor(parts[1]*Math.random())+1;
+                    diceToPrint.push(roll);
+                }
+            }
+            else {
+                msg.channel.send("No die rolled")
+                return;
+            }
+            let stringOfDice = "";
+            for (let diceRolled of diceToPrint) {
+                stringOfDice += `Rolling die. Rolled a ${diceRolled}\n`;
+            }
+            msg.channel.send(stringOfDice);
+        }
+        else {
+            msg.channel.send("No die rolled")
+        }
+    }
 });
 
 bot.on('clickMenu', async (menu) => {
     if (menu.clicker.user.id == latestMessageId) {
-        for (var i = 0; i < games.length; i++) {
-            child_process.exec("sudo systemctl stop "+games[i]["value"]+".service");
-        }
+        killAllServices();
         child_process.exec("sudo systemctl start "+menu.values[0]+".service");
         menu.channel.send("Starting "+menu.values[0]+". Please don't spam starting it up.");
     }
